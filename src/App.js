97b4514cf2252fc95e38c8537dbaa85505e3ff36
lukaszborowski <lukasz1borowski@gmail.com>
2021-05-24 18:57:06 +0200

    Initialize commit

---
+++ src/App.js
@@ -0,0 +1,135 @@
+import React, {Component, useState } from "react";
+import './App.css';
+import "./styles.scss";
+import "./buys.js";
+import "bootstrap";
+
+
+
+
+
+// Header
+
+const Header = () => {
+    return (
+        <section className="header__section">
+            <div className="header__container">
+                <h2>CS:GO Economy Keeper</h2>
+            </div>
+        </section>
+    )
+};
+
+// Player bar
+
+const Money = () => {
+    return (
+        <div className="money-bar">
+            <strong>Money</strong>
+        </div>
+    )
+}
+
+const Weapon = () => {
+    return (
+        <div className="weapon-primary">
+            <strong>Rifle</strong>
+        </div>
+    )
+}
+
+const Weaponsec = () => {
+    return (
+        <div className="weapon-secondary">
+            <strong>Pistol</strong>
+        </div>
+    )
+
+}
+
+const Equipment = () => {
+    return (
+        <div className="equip-bar">
+            <div className="armor-bar">armor
+            </div>
+            <div className="grenades-bar">
+                <div className="granade-low">Low granade
+                </div>
+                <div className="granade-med">Medium granade
+                </div>
+                <div className="granade-full">Full granade
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+const Buymenu = () => {
+    return (
+        <div className="buy-menu">
+
+        </div>
+    )
+}
+
+const Tplayer = () => {
+    return (
+        <div className="terro-player">
+            <h4>Terrorist Player</h4>
+            <Money/>
+            <Weapon/>
+            <Weaponsec/>
+            <Equipment/>
+            <Buymenu/>
+            <div className="player-action">
+                <button>Died</button>
+                <button>Frag/dropdown</button>
+            </div>
+        </div>
+    )
+
+}
+
+
+// Team
+
+const Terrorist = () => {
+    return (
+        <section className="terrorist-team">
+            <h3> Terrorist Team</h3>
+            <div className="terro-status">
+                <div className="terro-round-indycator">Round</div>
+                <div className="terro-loss-bonus">Loss Bonus: </div>
+            </div>
+            <div className="terrorist-team-bar">
+                <Tplayer/>
+                <Tplayer/>
+                <Tplayer/>
+                <Tplayer/>
+                <Tplayer/>
+            </div>
+            <div className="terrorist-round-decider">
+                <button className="terrorist-round-lost">Lost/dropdown=time/team died/bomb defused</button>
+                <button className="terrorist-round-win">Win/dropdown=CT killed/Bomb boom</button>
+            </div>
+
+        </section>
+
+    )
+}
+
+
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <Terrorist />
+        )
+    }
+
+}
+
+export default App;
---
+++ src/buys.js
@@ -0,0 +1,317 @@
+
+// Rifles
+
+const kalah = {
+    name: "AK-47",
+    cost: "2700",
+    killreward: "300",
+    icon: "../Icons/weapon_ak47.svg",
+    side: "T"
+};
+
+
+const galil = {
+    name: "GALIL AR",
+    cost: "1800",
+    killreward: "300",
+    icon: "../Icons/weapon_galilar.svg",
+    side: "T"
+};
+
+const m4a1 = {
+    name: "M4A1",
+    cost: "3100",
+    killreward: "300",
+    icon: "../Icons/weapon_m4a1.svg",
+    side: "CT"
+};
+
+const m4a1s = {
+    name: "M4A1 Silenced",
+    cost: "2900",
+    killreward: "300",
+    icon: "../Icons/weapon_m4a1_silencer.svg",
+    side: "CT"
+}
+
+const aug = {
+    name: "AUG",
+    cost: "3300",
+    killreward: "300",
+    icon: "../Icons/weapon_aug.svg",
+    side: "CT"
+};
+
+const famas = {
+    name: "FAMAS",
+    cost: "2250",
+    killreward: "300",
+    icon: "../Icons/weapon_famas.svg",
+    side: "CT"
+};
+
+const awp = {
+    name: "AWP",
+    cost: "4750",
+    killreward: "100",
+    icon: "../Icons/weapon_awp.svg"
+};
+
+const sg553 = {
+    name: "SG 553",
+    cost: "3000",
+    killreward: "300",
+    icon: "../Icons/weapon_sg556.svg",
+    side: "T"
+};
+
+const scar = {
+    name: "SCAR-20",
+    cost: "5000",
+    killreward: "300",
+    icon: "../Icons/weapon_scar20.svg",
+    side: "CT"
+}
+
+const g3sg1 = {
+    name: "G3SG1",
+    cost: "5000",
+    killreward: "300",
+    icon: "../Icons/weapon_g3sg1.svg",
+    side: "T"
+}
+
+const scout = {
+    name: "SSG 08",
+    cost: "1700",
+    killreward: "300",
+    icon: "../Icons/weapon_ssg08.svg"
+}
+
+// Pistols
+
+const deagle = {
+    name: "DESERT EAGLE",
+    cost: "700",
+    killreward: "300",
+    icon: "../Icons/weapon_deagle.svg"
+};
+
+
+const czauto = {
+    name: "CZ75 AUTO",
+    cost: "500",
+    killreward: "100",
+    icon: "../Icons/weapon_cz75a.svg"
+};
+
+
+
+const p250 = {
+    name: "P250",
+    cost: "300",
+    killreward: "300",
+    icon: "../Icons/weapon_p250.svg"
+};
+
+
+const usps = {
+    name: "USP Silenced",
+    cost: "200",
+    killreward: "300",
+    icon: "../Icons/weapon_usp_silencer.svg",
+    side: "CT"
+};
+
+const p2000 = {
+    name: "P2000",
+    cost: "200",
+    killreward: "300",
+    icon: "../Icons/weapon_hkp2000.svg",
+    side: "CT"
+};
+
+const glock = {
+    name: "GLOCK-18",
+    cost: "200",
+    killreward: "300",
+    icon: "../Icons/weapon_glock.svg"
+}
+
+const dual = {
+    name: "Dual Berettas",
+    cost: "400",
+    killreward: "300",
+    icon: "../Icons/weapon_elite.svg"
+}
+
+const fiveseven = {
+    name: "Five Seven",
+    cost: "500",
+    killreward: "300",
+    icon: "../Icons/weapon_fiveseven.svg",
+    side: "CT",
+}
+
+const tec9 = {
+    name: "TEC9",
+    cost: "500",
+    killreward: "300",
+    icon: "../Icons/weapon_tec9.svg",
+    side: "T"
+}
+
+// SMG
+
+const ump45 = {
+    name: "UMP-45",
+    cost: "1200",
+    killreward: "600",
+    icon: "../Icons/weapon_ump45.svg"
+};
+
+const mp9 = {
+    name: "MP9",
+    cost: "1250",
+    killreward: "600",
+    icon: "../Icons/weapon_mp9",
+    side: "CT"
+};
+
+const mac10 = {
+    name: "MAC-10",
+    cost: "1050",
+    killreward: "600",
+    icon: "../Icons/weapon_mac10.svg",
+    side: "T"
+};
+
+const mp7 = {
+    name: "MP7",
+    cost: "1500",
+    killreward: "600",
+    icon: "../Icons/weapon_mp7.svg"
+}
+
+const p90 = {
+    name: "P-90",
+    cost: "2350",
+    killreward: "300",
+    icon: "../Icons/weapon_p90.svg"
+}
+
+const biz = {
+    name: "PP-BIZON",
+    cost: "1400",
+    killreward: "600",
+    icon: "../Icons/weapon_bizon.svg"
+}
+
+const mp5 = {
+    name: "MP5-SD",
+    cost: "1500",
+    killreward: "600",
+    icon: "../Icons/weapon_"
+}
+
+// Heavy
+
+const nova = {
+    name: "NOVA",
+    cost: "1200",
+    killreward: "900",
+    icon: "../Icons/weapon_nova.svg"
+}
+
+const xm1014 = {
+    name: "XM1014",
+    cost: "2200",
+    killreward: "900",
+    icon: "../Icons/weapon_xm1014.svg"
+
+}
+
+const mag = {
+    name: "MAG-7",
+    cost: "1800",
+    killreward: "900",
+    icon: "../Icons/weapon_mag7.svg"
+}
+
+// Granades
+
+const incgrenade = {
+    name: "Incendiary Grenade",
+    cost: "600",
+    icon: "../Icons/weapon_incgrenade.svg",
+    side: "CT"
+}
+
+const molotov = {
+    name: "Molotov",
+    cost: "400",
+    icon: "../Icons/weapon_molotov.svg",
+    side: "T"
+}
+
+const decoy = {
+    name: "Decoy Grenade",
+    cost: "50",
+    icon: "../Icons/weapon_decoy.svg"
+}
+
+const flash = {
+    name: "Flash Grenade",
+    cost: "200",
+    icon: "../Icons/weapon_flashbang.svg"
+}
+
+const hegrenade = {
+    name: "HE Granade",
+    cost: "300",
+    icon: "../Icons/weapon_hegrenade.svg"
+}
+
+const smoke = {
+    name: "Smoke Grenade",
+    cost: "300",
+    icon: "../Icons/weapon_smokegrenade.svg"
+}
+
+// Gear
+
+const kevlar = {
+    name: "Kevlar Vest",
+    cost: "650",
+    icon: "../Icons/item_kevlar.svg"
+}
+
+const armor = {
+    name: "Kevlar + Helmet",
+    cost: "1000",
+    icon: "../Icons/item_assaultsuit.svg"
+}
+
+const defuser = {
+    name: "Defuse Kit",
+    cost: "400",
+    icon: "../Icons/item_defuser.svg",
+    side: "CT"
+}
+
+// Buy Tables T
+
+const terrpist = [deagle, glock, dual, tec9, p250, czauto ];
+const terrsmg = [mac10, mp7, ump45, p90, biz, mp5];
+const terrrifle = [kalah, galil, sg553, scout, awp, g3sg1];
+const terrgear = [kevlar, armor, molotov, decoy, flash, hegrenade, smoke ];
+
+// Buy Tables CT
+
+const ctpist = [deagle, glock, dual, p250, czauto, fiveseven, p2000, usps];
+const ctsmg = [ump45, mp9, mp7, p90, biz, mp5];
+const ctrifle = [m4a1, m4a1s, famas, aug, scout, awp, scar,];
+const ctgear = [ kevlar, armor, incgrenade, decoy, flash, hegrenade, smoke, defuser];
+
+const heavy = [nova, xm1014, mag];
+
